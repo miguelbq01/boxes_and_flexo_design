da743b6a1b61df1ef1cf3b20d7024f332bb133a8
Carlos Alberto Abril Lee <carlos.abril@docdigitales.com>
2019-05-22 23:36:39 -0700

    (Login) Add Jquery Functions for login validations

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
+    $('#login_form').fadeIn(1);
+
+
     $('.btn_iniciar_sesion').on('click', function() {
         if ($('#login_input_password').val() == "") {
-            alert();
+            $('#login_error_alert').slideDown();
+            $('#login_input_password').parent().addClass('input_error_validation');
+            setTimeout(function() {
+                $('#login_error_alert').slideUp();
+            }, 5000);
         }
+
+        if ($('#login_input_usuario').val() == "") {
+            $('#login_error_alert').slideDown();
+            $('#login_input_usuario').parent().addClass('input_error_validation');
+            setTimeout(function() {
+                $('#login_error_alert').slideUp();
+            }, 5000);
+        }
+
+    });
+    $('#login_input_usuario').on('focus', function() {
+        $('#login_input_usuario').parent().removeClass('input_error_validation');
+    });
+    $('#login_input_password').on('focus', function() {
+        $('#login_input_password').parent().removeClass('input_error_validation');
     });
 
+
+
+    $('.delete').on('click', function() {
+        $(this).parent().slideUp();
+
+    });
 });
